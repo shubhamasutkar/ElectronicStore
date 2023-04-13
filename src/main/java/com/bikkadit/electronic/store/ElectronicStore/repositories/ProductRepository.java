41778e06ff045b07444543661282977c32abc491
shubham asutkar <shubhamasutkar111@gmail.com>
2023-04-13 13:22:22 +0530

    changes done in ProductRepository

--- src/main/java/com/bikkadit/electronic/store/ElectronicStore/repositories/ProductRepository.java
+++ src/main/java/com/bikkadit/electronic/store/ElectronicStore/repositories/ProductRepository.java
@@ -1,6 +1,8 @@
 package com.bikkadit.electronic.store.ElectronicStore.repositories;
 
 import com.bikkadit.electronic.store.ElectronicStore.entities.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -8,6 +10,6 @@ import java.util.List;
 public interface ProductRepository extends JpaRepository<Product,Long> {
 
     //search
-   List <Product>findByTitleContaining(String subTitle);
-    List<Product> findByLiveTrue();
+    Page <Product>findByTitleContaining(String subTitle, Pageable pageable);
+    Page<Product> findByLiveTrue(Pageable pageable);
 }
